1d411d9975aaa972367e3925baa19f97cdacb12f
Quangang Sheng <shengquangang2015@163.com>
2019-05-16 21:09:43 +0800

    Nothing much.

--- src/gnssxx.hpp
+++ src/gnssxx.hpp
@@ -3,11 +3,17 @@
 
 #include <streambuf>
 
+#include "misc/utils.hpp"
 #include "misc/decimal.hpp"
+
 #include "time/unixtime.hpp"
+#include "time/utc.hpp"
+#include "time/gps.hpp"
+#include "time/tai.hpp"
 #include "time/jd.hpp"
 
 namespace gnssxx {
+
 }
 
 #endif // GNSSXX_GNSSXX_HPP_INCLUDED
--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,42 +1,62 @@
 #include <cstdlib>
 #include <iostream>
 #include <sstream>
+#include <ctime>
 
-#include "../src/misc/utils.hpp"
 #include "../src/gnssxx.hpp"
 
 namespace gnssxx {
-    template <typename CharT,
-             typename Traits = std::char_traits<CharT>,
-             typename Alloc = std::allocator<CharT> >
+    template <
+                typename CharT,
+                typename Traits = std::char_traits<CharT>,
+                typename Alloc = std::allocator<CharT>
+                    >
     class basic_simplebuf : public std::basic_streambuf<CharT> {
-        public:
-            typedef typename Traits::int_type int_type;
-        public:
-            virtual int_type underflow() {
-                return 0;
-            }
+    public:
+        typedef typename Traits::int_type int_type;
 
-            virtual int_type overflow(int_type ch = Traits::eof()) {
-                return 0;
-            }
+    public:
+        virtual int_type underflow() { return 0; }
 
-            virtual int_type sync() {
-                return 0;
-            }
+        virtual int_type overflow(int_type ch = Traits::eof()) { return 0; }
+
+        virtual int_type sync() { return 0; }
     };
 
     typedef basic_simplebuf<char> simplebuf;
     typedef basic_simplebuf<wchar_t> wsimplebuf;
 
-    class epoch_time {
-    };
+    class epoch_time {};
+
+    class unix_time {};
+
+    class gps_time {};
+
+    class tai_time {};
+
+    class utc_time {};
+
+    namespace time {
+        time_t gps() { return 0; }
+        time_t utc() { return 0; }
+        time_t tai() { return 0; }
+        time_t tai(std::time_t const &t) { return t; }
+
+        time_t unix() {
+            std::time_t t = 0;
+            if (std::time(&t) == static_cast<std::time_t>(-1))
+                throw std::runtime_error("Can't get time from c library!");
+            return t;
+        }
+    }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     using namespace std;
     using namespace gnssxx;
 
     cout << static_cast<time_t>(unixtime::now()) << endl;
+    cout << time::utc() << endl;
+    cout << time::tai(3) << endl;
     return EXIT_SUCCESS;
 }
