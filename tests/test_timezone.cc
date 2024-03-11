0c549666ddb77d98c51aa4038a41bffd20269e7a
Quangang Sheng <shengquangang2015@163.com>
2024-03-11 17:46:19 +0800

    refactor unit test

--- tests/test_timezone.cc
+++ tests/test_timezone.cc
@@ -5,16 +5,11 @@
 
 #include "gnssxx.hpp"
 
-int main(int argc, char *argv[]) try {
+#include "gtest/gtest.h"
+
+TEST(timezone_test, current_tz) {
     using gnssxx::time::unixtime;
     using gnssxx::time::timezone;
     std::cout << "now = " << static_cast<time_t>(unixtime::now()) << std::endl;
     std::cout << "tz = " << timezone::offset() << std::endl;
-    return EXIT_SUCCESS;
-} catch (std::exception const &e) {
-    std::cerr << "[C++ exception] " << e.what() << std::endl;
-    return EXIT_FAILURE;
-} catch (...) {
-    std::cerr << "[C++ exception] " << "<UNKNOWN CAUSE>" << std::endl;
-    return EXIT_FAILURE;
 }
