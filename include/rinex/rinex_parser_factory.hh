7b038e1716e98b24a4da29628f715afbfb4b20cf
Quangang Sheng <shengquangang2015@163.com>
2024-04-19 19:09:27 +0800

    add internal naive storage for parsers

--- include/rinex/rinex_parser_factory.hh
+++ include/rinex/rinex_parser_factory.hh
@@ -1,6 +1,8 @@
 #ifndef GNSSXX_RINEX_RINEX_PARSER_FACTORY_HH_INCLUDED
 #define GNSSXX_RINEX_RINEX_PARSER_FACTORY_HH_INCLUDED
 
+#include <string>
+#include <vector>
 #include <map>
 
 #include "rinex/rinex_parser.hh"
@@ -13,19 +15,23 @@ class rinex_parser_factory {
     public:
         typedef std::shared_ptr<rinex_parser> parser_ptr;
         typedef std::map<rinex_file_version, parser_ptr> map_type;
+        typedef std::vector<map_type> typed_parsers;
 
         static std::shared_ptr<rinex_parser_factory> instance();
 
         parser_ptr get_parser(
                 rinex_file_version const&,
-                rinex_file_type const& type);
+                rinex_file_type const&) const;
         void set_parser(
                 rinex_file_version const&,
-                rinex_file_type const &type,
-                parser_ptr parser);
+                rinex_file_type const &,
+                parser_ptr);
+        map_type::const_iterator get_compat_parsers(
+                rinex_file_version const&,
+                rinex_file_type const&) const;
     protected:
     private:
-        map_type _M_parsers;
+        typed_parsers _M_typed_parsers;
 };
 
 } // namespace rinex
--- src/rinex/rinex_parser_factory.cc
+++ src/rinex/rinex_parser_factory.cc
@@ -22,16 +22,37 @@ rinex_parser_factory::instance() {
 rinex_parser_factory::parser_ptr
 rinex_parser_factory::get_parser(
         rinex_file_version const &version,
-        rinex_file_type const &type) {
-    // TODO not implemented
-    return NULL;
+        rinex_file_type const &type) const {
+    size_t index = static_cast<size_t>(type);
+    if (index < 0 || index >= _M_typed_parsers.size())
+        return nullptr;
+    map_type::const_iterator it = _M_typed_parsers[index].find(version);
+    if (it == _M_typed_parsers[index].end())
+        return nullptr;
+    return it->second;
 }
 
 void rinex_parser_factory::set_parser(
         rinex_file_version const &version,
         rinex_file_type const &type,
         std::shared_ptr<rinex_parser> parser) {
-    // TODO not implemented
+    size_t index = static_cast<size_t>(type);
+    if (index < 0 || index >= _M_typed_parsers.size())
+        _M_typed_parsers.resize(index + 1);
+    _M_typed_parsers[index][version] = parser;
+}
+
+rinex_parser_factory::map_type::const_iterator
+rinex_parser_factory::get_compat_parsers(
+        rinex_file_version const &version,
+        rinex_file_type const &type) const {
+    size_t index = static_cast<size_t>(type);
+    if (index < 0 || index >= _M_typed_parsers.size())
+        return map_type::const_iterator();
+    map_type::const_iterator it = _M_typed_parsers[index].lower_bound(version);
+    if (it == _M_typed_parsers[index].end())
+        return map_type::const_iterator();
+    return it;
 }
 
 } // namespace rinex
